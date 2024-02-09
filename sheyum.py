29be179227797b0878e2194d8d8d21c3eb278286
Code_Paradox <seyumhossain@gmail.com>
2024-02-09 16:00:17 +0600

    new commit

--- sheyum.py
+++ sheyum.py
@@ -4,26 +4,41 @@ import sqlite3
 
 conn = sqlite3.connect('database.db')
 cursor = conn.cursor()
-
 app = FastAPI()
 
 class Item(BaseModel):
     name: str
     description: str = None
 
+def convert(books):
+    my_dict={}
+    book_list=[]
+
+    for value in books:
+        my_dict["id"]=value[0]
+        my_dict["title"]=value[1]
+        my_dict["author"]=value[2]
+        my_dict["genre"]=value[3]
+        book_list.append(my_dict)
+        my_dict={}
+
+    return book_list
+
 @app.get("/items")
 async def read_item():
     cursor.execute("SELECT * FROM students")
     item = cursor.fetchall()
+    book_list=convert(item) # Calling Function
+
 
     if item is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item not found")
     
-    return {"item": item}
+    return {"items": book_list}
 
 @app.get("/items/{itemid1}/{itemid2}")
 async def read_item(itemid1 : int, itemid2 : int):
-    cursor.execute("SELECT * FROM items")
+    cursor.execute("SELECT * FROM students")
     item = cursor.fetchone()
 
     if item is None:
